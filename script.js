37e0d6f6bc022afc526b13968ad7edf9c8ea57e8
Phoenix <75660894+PhoenixTheCoder@users.noreply.github.com>
2021-09-29 16:37:38 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,75 @@
+
+let upperCaseFancy = {
+"A": "𝓐",
+"B": "𝓑",
+"C": "𝓒",
+"D": "𝓓",
+"E": "𝓔",
+"F": "𝓕",
+"G": "𝓖",
+"H": "𝓗",
+"I": "𝓘",
+"J": "𝓙",
+"K": "𝓚",
+"L": "𝓛",
+"M": "𝓜",
+"N": "𝓝",
+"O": "𝓞",
+"P": "𝓟",
+"Q": "𝓠",
+"R": "𝓡",
+"S": "𝓢",
+"T": "𝓣",
+"U": "𝓤",
+"V": "𝓥",
+"W": "𝓦",
+"X": "𝓧",
+"Y": "𝓨",
+"Z": "𝓩"
+};
+
+let lowerCaseFancy = {
+"a": "𝓪",
+"b": "𝓫",
+"c": "𝓬",
+"d": "𝓭",
+"e": "𝓮",
+"f": "𝓯",
+"g": "𝓰",
+"h": "𝓱",
+"i": "𝓲",
+"j": "𝓳",
+"k": "𝓴",
+"l": "𝓵",
+"m": "𝓶",
+"n": "𝓷",
+"o": "𝓸",
+"p": "𝓹",
+"q": "𝓺",
+"r": "𝓻",
+"s": "𝓼",
+"t": "𝓽",
+"u": "𝓾",
+"v": "𝓿",
+"w": "𝔀",
+"x": "𝔁",
+"y": "𝔂",
+"z": "𝔃"
+};
+
+MPP.chat.send = (msg) => {
+  if (/[a-z]/.test(msg)) {
+    Object.keys(lowerCaseFancy).forEach(f => {
+      msg = msg.replaceAll(f, lowerCaseFancy[f]);
+    })
+  }
+  if (/[A-Z]/.test(msg)) {
+    Object.keys(upperCaseFancy).forEach(f => {
+      msg = msg.replaceAll(f, upperCaseFancy[f]);
+    })
+  }
+    MPP.client.sendArray([{
+        m: 'a',
+        message: msg
+    }]);
+}
